7d5181504df71ce7bdc134cb23efe952e9baeaf3
SamihaHatem <98450607+SamihaHatem@users.noreply.github.com>
2022-03-23 18:02:12 +0200

    version 3

--- src/app/temprature.pipe.ts
+++ src/app/temprature.pipe.ts
@@ -7,11 +7,15 @@ export class TempraturePipe implements PipeTransform {
 
   transform(value: any,unit:string) {
     var temp
-    if(value){
-      if(unit='Fahrenheit')
-        temp=(value * (9/5)) + 32
-      if(unit='Celsius')
-      temp=(value-32)/ (9/5)
+    if(value && !isNaN(value)){
+
+      if(unit=='Celsius'){
+        temp=(value-32)/1.8
+     }
+     if(unit=='Fahrenheit'){
+        temp=(value*1.8)+32
+     }
+    
     return temp
     }
     return null;
